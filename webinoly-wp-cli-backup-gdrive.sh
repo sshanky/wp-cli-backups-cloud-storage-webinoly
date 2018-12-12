61db73f8039c6f7b29e61860ff6b18abe2883ab0
sshanky <sshank@gmail.com>
2018-12-12 13:29:07 -0700

    Replaced original script with webinoly experimental script

--- webinoly-wp-cli-backup-gdrive.sh
+++ webinoly-wp-cli-backup-gdrive.sh
@@ -1,28 +1,42 @@
-#!/usr/bin/env bash
-# Source: https://guides.wp-bullet.com
-# Author: Mike
+#!/bin/bash
+# Based on script by Mike from: https://guides.wp-bullet.com
+# 20181202 updated for webinoly to handle output from 'site -list' command
 
 #define local path for backups
-BACKUPPATH="/tmp/backups"
+BACKUPPATH="/path/to/gdrive-backup-tmp/folder"
 
 #define remote backup path
-BACKUPPATHREM="WP-Bullet-Backups"
+BACKUPPATHREM="folder-name-on-gdrive"
 
-#path to WordPress installations
+#path to WordPress installations, no trailing slash
 SITESTORE="/var/www"
 
 #date prefix
 DATEFORM=$(date +"%Y-%m-%d")
 
 #Days to retain
-DAYSKEEP=7
+DAYSKEEP=14
 
 #calculate days as filename prefix
 DAYSKEPT=$(date +"%Y-%m-%d" -d "-$DAYSKEEP days")
 
 #create array of sites based on folder names
-SITELIST=($(ls -d $SITESTORE/* | awk -F '/' '{print $NF}'))
+echo Generating site list
 
+# webinoly site -list command returns lines that start with hyphens and spaces, along with control chars, which need to be removed
+# SED s/[\x01-\x1F\x7F]//g removes control characters
+# SED s/.{7}// removes first seven chars and s/.{5}$// removes last 5 chars
+# SED /^\s*$/d removes blank lines
+# removing characters http://www.theunixschool.com/2014/08/sed-examples-remove-delete-chars-from-line-file.html
+# removing empty lines https://stackoverflow.com/questions/16414410/delete-empty-lines-using-sed
+SITELIST=($(/usr/bin/site -list | sed -r "s/[\x01-\x1F\x7F]//g;s/.{7}//;s/.{5}$//;/^\s*$/d"))
+#print site list
+echo Site list
+echo ---
+for SITE in ${SITELIST[@]}; do
+echo "$SITE"
+done
+echo ---
 #make sure the backup folder exists
 mkdir -p $BACKUPPATH
 
@@ -34,28 +48,34 @@ BACKUPSID=$(gdrive list --no-header | grep $BACKUPPATHREM | grep dir | awk '{ pr
     fi
 
 #start the loop
+echo Starting
 for SITE in ${SITELIST[@]}; do
+    echo ----------
+    echo Working on $SITE
     #delete old backup, get folder id and delete if exists
     OLDBACKUP=$(gdrive list --no-header | grep $DAYSKEPT-$SITE | grep dir | awk '{ print $1}')
     if [ ! -z "$OLDBACKUP" ]; then
         gdrive delete $OLDBACKUP
     fi
-    
+
     # create the local backup folder if it doesn't exist
     if [ ! -e $BACKUPPATH/$SITE ]; then
         mkdir $BACKUPPATH/$SITE
     fi
 
-    #entire the WordPress folder
-    cd $SITESTORE/$SITE
-  
+    #enter the WordPress folder
+    #added "htdocs" per ee structure
+    cd $SITESTORE/$SITE/htdocs
+
     #back up the WordPress folder
-    tar -czf $BACKUPPATH/$SITE/$DATEFORM-$SITE.tar.gz .
+    #tar -czf $BACKUPPATH/$SITENAME/$SITE/$DATEFORM-$SITE.tar.gz .
+    zip -r --quiet $BACKUPPATH/$SITENAME/$SITE/$DATEFORM-$SITE.zip .
     #back up the WordPress database, compress and clean up
-    wp db export $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql --all-tables --single-transaction --quick --lock-tables=false --allow-root --skip-themes --skip-plugins
-    cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | gzip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+    wp db export $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql --all-tablespaces --single-transaction --quick --lock-tables=false --allow-root --skip-themes --skip-plugins
+    #cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | gzip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+    cat $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql | zip > $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.zip
     rm $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql
-    
+
     #get current folder ID
     SITEFOLDERID=$(gdrive list --no-header | grep $SITE | grep dir | awk '{ print $1}')
 
@@ -66,9 +86,12 @@ for SITE in ${SITELIST[@]}; do
     fi
 
     #upload WordPress tar
-    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+    #gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.tar.gz
+    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.zip
     #upload wordpress database
-    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.tar.gz
+    #gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.gz
+    gdrive upload --parent $SITEFOLDERID --delete $BACKUPPATH/$SITE/$DATEFORM-$SITE.sql.zip
+
 done
 
 #Fix permissions
